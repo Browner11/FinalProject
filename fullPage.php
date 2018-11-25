59f8d825874e4af8bc6d184691b9dadca79d3a58
Bryce Brown <brycebrown91@hotmail.com>
2018-11-24 19:40:43 -0600

    Implemented comment section
    - Implemented comment section
    - Added "DateCreated" and "DateEdited" columns to book table
    - Layout changes to full page for books

--- fullPage.php
+++ fullPage.php
@@ -10,6 +10,12 @@
      // Execution on the DB server is delayed until we execute(). 
      $statement->execute();
 
+     $ISBN = $_GET['ISBN'];
+
+     $commentQuery = "SELECT * FROM comment WHERE ISBN == $ISBN";
+     $commentStatement = $db->prepare($commentQuery);
+     $commentStatement->execute();
+
 
 ?>
 
@@ -21,24 +27,37 @@
 </head>
 <body>
 	<?php include('nav.php'); ?>
-	<fieldset>
-        <?php if ($statement->rowCount() == 0) :?>
-        	<h1>No tweets found.</h1>
+    <div class="bookcontent">
+        <div class="bookimage">    	
+            <?php if ($statement->rowCount() == 0) :?>
+            	<h1>No book found.</h1>
+            <?php else :?>
+                <?php while($row = $statement->fetch()):?>
+                	<?php if ($row['ISBN'] == $_GET['ISBN']) : ?>
+                        <img src="uploads/<?=$row['CoverImg'] ?>">                      	
+        </div>
+        <div class="bookdetails">
+                        <ul>
+                            <li>
+                              <?= "<h4>" . $row['Title'] . "</h4>";?>
+                              <?= "<p>" . $row['Author'] . "</p>";?>
+                              <?= "<p>" . $row['Genre'] . "</p>";?>
+                              <?= "<br>";?>
+                            </li>
+                        </ul>
+                    <?php endif?>
+                <?php endwhile ?>    
+             <?php endif?>             
+        </div>
+    </div>
+    <div class="comments">
+        <?php if ($commentStatement->rowCount() == 0) :?>
+            <h1>No comments found.</h1>
         <?php else :?>
-            <?php while($row = $statement->fetch()):?>
-            	<?php if ($row['ISBN'] == $_GET['ISBN']) : ?>
-                    <img src="uploads/<?=$row['CoverImg'] ?>">
-                    <ul>
-	                    <li>
-	                      <?= "<h4>" . $row['Title'] . "</h4>";?>
-	         		      <?= "<p>" . $row['Author'] . "</p>";?>
-	                      <?= "<p>" . $row['Genre'] . "</p>";?>
-	                      <?= "<br>";?>
-	                    </li>
-	                </ul>
-            	<?php endif?>
-             <?php endwhile ?>    
-         <?php endif ?> 
-    </fieldset>
+                <?php while($commentRow = $commentStatement->fetch()):?>
+
+                <?php endwhile ?>
+        <?php endif ?>        
+    </div>
 </body>
 </html>
